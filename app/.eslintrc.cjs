c025b4d6a6f0e873a006320695bd4dac133c4dff
kiiiyo <kiiiyo.jp@gmail.com>
2022-11-05 10:07:30 +0900

    chore: modify config file

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -15,6 +15,6 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {},
 }
